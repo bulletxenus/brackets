8aca66d905fe9c53526d7399755a87e9c953b16e
Dmitry Khamitsevich <bullet.xenus@gmail.com>
2020-09-08 22:58:49 +0300

    fix: realize function check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+    let arrStr = str.split("");
+
+        for (let i = 0; i < arrStr.length; i++) {
+
+            for (let j = 0; j < bracketsConfig.length; j++) {
+                if (arrStr[i] === bracketsConfig[j][0] && arrStr[i+1] === bracketsConfig[j][1]) {
+                    arrStr.splice(i, 2);
+                    i = -1;
+                }
+            }
+        }
+        return arrStr.length === 0;
 }
